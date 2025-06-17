99bc1374633fe3b6a0b4ecef00dfc7dd1ba6d620
LavanyaBanga <lavanyabanga14@gmail.com>
2025-06-17 15:29:28 +0530

    hello

---
+++ index1.js
@@ -0,0 +1,26 @@
+var age=25;
+ if(true){
+     console.log(age);
+ }
+ function solve(){
+     var age=25;
+    console.log(age);
+ }
+ console.log(age);
+ solve();
+var age=25;age=21;
+var age=90;
+console.log(age);
+
+
+let age=25;
+ if(true){     let age=30;
+     console.log(age);
+     age=35;
+     console.log(age);
+ }
+ if(true){
+     const age=30;
+     
+     console.log(age);
+ }
